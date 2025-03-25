effc33793c1b37ea5d8facc7ce293763fb0f290e
Tilo Wiklund <tilo.wiklund@sensmetry.com>
2025-03-25 09:34:38 +0100

    Add generate-token example utility

---
+++ example/generate_access_token.py
@@ -0,0 +1,40 @@
+import os
+
+from eoepca_security import (
+    request_oidcutil,
+    OIDCUtil,
+    ClientCredentials,
+    RefreshToken,
+)
+
+OPEN_ID_CONNECT_URL = os.environ.get("OPEN_ID_CONNECT_URL")
+if OPEN_ID_CONNECT_URL is None:
+    exit("Please set OPEN_ID_CONNECT_URL")
+
+OPEN_ID_CONNECT_CLIENT_ID = os.environ.get("OPEN_ID_CONNECT_CLIENT_ID")
+if OPEN_ID_CONNECT_CLIENT_ID is None:
+    exit("Please set OPEN_ID_CONNECT_CLIENT_ID")
+
+OPEN_ID_CONNECT_CLIENT_SECRET = os.environ.get("OPEN_ID_CONNECT_CLIENT_SECRET")
+if OPEN_ID_CONNECT_CLIENT_SECRET is None:
+    exit("Please set OPEN_ID_CONNECT_CLIENT_SECRET")
+
+OPEN_ID_CONNECT_AUDIENCE = os.environ.get("OPEN_ID_CONNECT_AUDIENCE")
+if OPEN_ID_CONNECT_AUDIENCE is None:
+    exit("Please set OPEN_ID_CONNECT_AUDIENCE")
+
+OPEN_ID_REFRESH_TOKEN = os.environ.get("OPEN_ID_REFRESH_TOKEN")
+if OPEN_ID_REFRESH_TOKEN is None:
+    exit("Please set OPEN_ID_REFRESH_TOKEN")
+
+client_credentials = ClientCredentials(
+    OPEN_ID_CONNECT_CLIENT_ID, OPEN_ID_CONNECT_CLIENT_SECRET
+)
+
+new_refresh_token, new_auth_token = request_oidcutil(OPEN_ID_CONNECT_URL).refresh_auth_token(
+    client_credentials,
+    RefreshToken(OPEN_ID_REFRESH_TOKEN),
+)
+
+#print(f"New refresh token:\n\n{new_refresh_token.raw}\n\nAuth token\n\n{new_auth_token.raw}\n")
+print(new_auth_token.raw)
